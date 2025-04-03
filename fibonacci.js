2578346528ddb6ae3b05da74c923572c9b46d5ad
JANNIE02 <BIRUNGI.JANNIE@STRATHMORE.EDU>
2025-04-03 14:07:31 +0300

    added new test cases to bring a fail

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,8 @@
 function fibonacci(n) {
-    if (n === 0) 
-        return 0;
+    if (n === 0) return 0;
+        if (n === 1) return 2;  // This will fail the test for Fibonacci of 1
+    if (n === 2) return 3;  // This will fail the test for Fibonacci of 2
+
+      
 }
-module.exports = fibonacci; 
+module.exports = fibonacci;
--- test.js
+++ test.js
@@ -1,11 +1,19 @@
-// Test cases for Fibonacci
-
-// Jannie Birungi, Micheal Asiimwe
+// test.js
 const fibonacci = require('./fibonacci');
 const assert = require('chai').assert;
 
 describe('Fibonacci function', function() {
+  
   it('should return 0 for Fibonacci of 0', function() {
-    assert.equal(fibonacci(0), 0);
+    assert.equal(fibonacci(0), 0);  // This test will pass
   });
-})
+
+  it('should return 1 for Fibonacci of 1', function() {
+    assert.equal(fibonacci(1), 1);  // This test will fail because the fibonacci function returns 2 instead of 1
+  });
+
+  it('should return 1 for Fibonacci of 2', function() {
+    assert.equal(fibonacci(2), 1);  // This test will fail because the fibonacci function returns 3 instead of 1
+  });
+
+});
